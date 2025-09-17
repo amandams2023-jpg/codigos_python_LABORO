e77866a4bc9f531bc904a9da1824aeb03221a0a6
amandams2023-jpg <amandams2023@gmail.com>
2025-09-17 20:16:56 -0300

    Trabalhando com for em python

---
+++ repeticao.py
@@ -0,0 +1,25 @@
+""""
+for(etapa1; etapa2; etapa3){
+
+}
+
+"""
+#range() não contar o valor final, ele ignoraa, se eu quiser exibir o valor final devo colocar.
+
+"""#Exemplo 1
+for contador in range (1,101):
+    print (contador)"""
+
+print("-" * 50)
+
+
+ #Exemplo 2
+ # o -1 inidica que o intervalo irá de -1 em -1, isto é o passo a passo do valor inicial até o valor final.
+"""for contador in range(10,0,-1):
+  print(contador,end=" ")   #Função para exibição"""
+
+ #Exemplo 3  
+for contador in range(0,20,2):
+ print(contador, end=", ")
+ print("-" * 20)
+
