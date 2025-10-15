27b83973e39595b78fba4e36027aec403250490a
amandams2023-jpg <amandams2023@gmail.com>
2025-10-15 19:30:38 -0300

    subir a tupla

---
+++ tupla.py
@@ -0,0 +1,14 @@
+# Criando uma tupla 
+frutas = ("Banana", "Maça", "Laranja", "Uva")
+
+#print(type(frutas))
+
+print(frutas)
+
+print(frutas[0])
+
+# Exibit todas as frutas 
+
+for item in frutas:
+    print(item)
+
